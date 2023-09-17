7e3e825ab737ce7aaf88dd7658d7a1abe73d498b
carlgira <carlgira@gmail.com>
2023-09-17 10:05:42 +0200

    Remove background node

--- comfyui/ImageToMaskNode.py
+++ comfyui/ImageToMaskNode.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+from PIL import Image
+import rembg
 import cv2
 
 class ImageColorsToMask:
@@ -23,24 +25,45 @@ class ImageColorsToMask:
     FUNCTION = "image_to_mask"
 
     def image_to_mask(self, image, r_min, g_min, b_min, r_max, g_max, b_max):
-        temp = (torch.clamp(image[0], 0, 1.0) * 255.0).round().to(torch.int)
-        #temp = torch.bitwise_left_shift(temp[:,:,0], 16) + torch.bitwise_left_shift(temp[:,:,1], 8) + temp[:,:,2]
-        #mask = torch.where((temp >= color_min) & (temp <= color_max), 255, 0).float()
-        #mask = torch.where((temp[:,:,0] > r_min) & (temp[:,:,0] < r_max) & (temp[:,:,1] > g_min) & (temp[:,:,1] < g_max) & (temp[:,:,2] > b_min) & (temp[:,:,2] < b_max), 255, 0).float()
         raw_image = 255. * image[0].numpy()
         image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2BGR)
         lower_bound = np.array([r_min, g_min, b_min])
         upper_bound = np.array([r_max, g_max, b_max])
-        hsv_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(raw_image, lower_bound, upper_bound)
         inverted_mask = cv2.bitwise_not(mask)
         alpha_channel = np.ones(mask.shape, dtype=mask.dtype) * 255
         alpha_channel = cv2.bitwise_and(alpha_channel, alpha_channel, mask=inverted_mask)
-        #mask = cv2.merge((image, alpha_channel))
         mask = torch.tensor(mask > 0).float()
         
         return (mask,)
-    
+
+class RemoveBackgroundAndMask:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+                "required": {
+                    "image": ("IMAGE",),
+                }
+        }
+
+    CATEGORY = "image"
+
+    RETURN_TYPES = ("MASK",)
+    FUNCTION = "remove_background_image_to_mask"
+
+    def remove_background_image_to_mask(self, image):
+        raw_image = 255 * image[0].numpy()
+        pil_image = Image.fromarray(raw_image.astype(np.uint8))
+        output = rembg.remove(pil_image)
+        output_array = np.array(output)
+        gray = cv2.cvtColor(output_array, cv2.COLOR_BGR2GRAY)
+        mask = torch.tensor(gray == 0).float()
+        
+        return (mask,)
+
+
+
 NODE_CLASS_MAPPINGS = {
     "ImageColorsToMask": ImageColorsToMask,
+    "RemoveBackgroundAndMask": RemoveBackgroundAndMask,
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,7 @@
 instance_name       = "genai-photobooth"
 instance_count = 1
-instance_shape      = "VM.GPU2.1"
+instance_shape      = "VM.GPU.A10.1"
 ssh_public_key_path = "server.key.pub"
 subnet_cidr         = "10.0.0.0/24"
 vcn_cidr            = "10.0.0.0/16"
-region = "eu-frankfurt-1"
+region = "eu-paris-1"
